ad916e8d9dc0ce3dd352996f5dc9c718abac8b4d
ScalesAir <scales0689@gmail.com>
2021-03-05 16:55:13 +0600

    Домашнее задание №2-2 "Радиус круга"

--- prob.py
+++ prob.py
@@ -1,34 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# Есть словарь координат городов
+# Есть значение радиуса круга
+radius = 42
 
-from pprint import pprint
-sites = {
-    'Moscow': (550, 370, 'Москвы',),
-    'London': (510, 510, "Лондона"),
-    'Paris': (480, 480, "Парижа"),
-}
+# Выведите на консоль значение площади этого круга с точностю до 4-х знаков после запятой
+# подсказки:
+#       формулу можно подсмотреть в интернете,
+#       пи возьмите равным 3.1415926
+#       точность указывается в функции round()
+area = 3.1415926 * (radius ** 2)
+float(area)
+print(round(area, 4))
 
-# Составим словарь словарей расстояний между ними
-# расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+# Далее, пусть есть координаты точки
+point = (23, 34)
+# где 23 - координата х, 34 - координата у
 
-moscow = sites['Moscow']
-london = sites['London']
-paris = sites['Paris']
-moscow_london = (moscow[0]-london[0]) ** 2 + (moscow[1]-london[1]) ** 2
-london_paris = (london[0]-paris[0]) ** 2 + (london[1]-paris[1]) ** 2
-moscow_paris = (moscow[0]-paris[0]) ** 2 + (moscow[1]-paris[1]) ** 2
-distances['Moscow'] = {}
-distances['Moscow']['London'] = moscow_london
-distances['Moscow']['Paris'] = moscow_paris
-distances['London'] = {}
-distances['London']['Moscow'] = moscow_london
-distances['London']['Paris'] = london_paris
-distances['Paris'] = {}
-distances['Paris']['Moscow'] = moscow_paris
-distances['Paris']['London'] = london_paris
-pprint(distances)
+# Если точка point лежит внутри того самого круга (radius = 42), то выведите на консоль True,
+# Или False, если точка лежит вовне круга.
+# подсказки:
+#       нужно определить расстояние от этой точки до начала координат (0, 0)
+#       формула так же есть в интернете
+#       квадратный корень - это возведение в степень 0.5
+#       операции сравнения дают булевы константы True и False
 
-print('Расстояние от', moscow[2], 'до', london[2], '=', distances['Moscow']['London'], 'км.')
+height = (point[0]**2 + point[1]**2) ** 0.5
+# print("Расстояние до точки от начала координат равно %.4f" % height)
+print(height < radius)
+
+# Аналогично для другой точки
+point_2 = (30, 30)
+# Если точка point_2 лежит внутри круга (radius = 42), то выведите на консоль True,
+# Или False, если точка лежит вовне круга.
+height = (point_2[0]**2 + point_2[1]**2) ** 0.5
+# print("Расстояние до точки от начала координат равно %.4f" % height)
+print(height < radius)
+
+# Пример вывода на консоль:
+#
+# 77777.7777
+# False
+# False
